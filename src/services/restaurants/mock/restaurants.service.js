eafe9062450f6482af7ba1570f2694069cc21dc4
Kripesh <kg9773092+github@gmail.com>
2026-01-24 16:47:18 +0545

    transforming the data that we got from api

--- src/services/restaurants/mock/restaurants.service.js
+++ src/services/restaurants/mock/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from './index';
+import camelize from 'camelize'; // read docs about camelize
 
 // using mocks and promisifying them just to mimic the google maps api call.
 export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
@@ -11,8 +12,32 @@ export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
   });
 };
 
+const restaurantsTransform = ({ results = [] }) => {
+  // we're doing this because we don't get isClosedTemporarily and isOpenNow property from the api
+  // that we need in our restaurant card component.
+  // so, we're deriving those properties based on what api is returning
+  // why we're doing this?
+  // because google maps api returns the data in these format, so to compensate it, we're transforming our data. Simple!
+  const mappedResults = results.map(restaurant => {
+    return {
+      ...restaurant,
+      isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARILY',
+      isOpenNow:
+        restaurant.opening_hours && restaurant.opening_hours.open_now
+          ? true
+          : false,
+    };
+  });
+
+  const newResult = camelize(mappedResults);
+  return newResult;
+};
+
 restaurantsRequest()
+  .then(restaurantsTransform)
   .then(result => {
-    console.log(result);
+    result.map(restaurant => {
+      console.log(restaurant.isOpenNow, restaurant.isClosedTemporarily);
+    });
   })
   .catch(err => console.log(err));
